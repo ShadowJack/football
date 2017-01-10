cbe52ba81d630775d83300bbca33551d995866cb
shadowjack <kostyap@hotmail.ru>
2017-01-10 21:27:42 +0300

    Add PeerJS

--- web/static/js/game_lobby.js
+++ web/static/js/game_lobby.js
@@ -1,17 +1,27 @@
 let GameLobby = {
-  guardian_token: "",
+  guardianToken: "",
+  peer: null,
+  connections: [],
 
   init(socket, token, lobbyId) {
-    this.guardian_token = token
+    this.guardianToken = token
     socket.connect({guardian_token: token})
 
-    let gameChannel = socket.channel(`game_lobby:${lobbyId}`, {})
+    // Create a phoenix channel
+    this.gameChannel = socket.channel(`game_lobby:${lobbyId}`, {})
 
-    gameChannel.join()
+    // Create a PeerJS peer
+    this.peer = new Peer('unique_user_id', {key: 'rrj0sxbt1e3a0pb9'})
+    //TODO: handle presence events
+    //TODO: when received the first list of users after connection, fill connections
+    //TODO: on new user connected to peer, add it to connections list
+
+    this.gameChannel.join()
       .receive("ok", ({lobby}) => {
         $("#LobbyName").text(lobby.name)
       })
       .receive("error", resp => console.log("Cannot connect to game lobby: ", resp))
+
   }
 }
 
